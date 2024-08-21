8456cd5315652e33821d201fb4f267e38eeabd5c
reza mohseni <155166979+rezaamohseni@users.noreply.github.com>
2024-08-22 00:01:58 +0330

    admin.py

--- root/admin.py
+++ root/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(SpecialService)
+admin.site.register(Business_Card_Portfolios)
+admin.site.register(ContactUs)
+admin.site.register(Portfolios_children)
